975983071772fc7e9adfb7aaa3b8d400f74c1372
FelipeTapia9 <feli.tapias@alumnos.duoc.cl>
2023-10-17 15:55:15 -0300

    duplicado por rut

--- registrar.php
+++ registrar.php
@@ -1,46 +1,51 @@
 <?php
 include("conexion.php");
 
-
-if (isset($_POST['guardar'])){
-    if(
-    
+if (isset($_POST['guardar'])) {
+    if (
         strlen($_POST['nombre_apellido']) >= 1 &&
         strlen($_POST['alias']) >= 1 &&
         strlen($_POST['rut']) >= 1 &&
-        strlen($_POST['email']) >= 1 && 
+        strlen($_POST['email']) >= 1 &&
         strlen($_POST['region']) >= 1 &&
         strlen($_POST['comuna']) >= 1 &&
         strlen($_POST['candidato']) >= 1
-        
-    ){
+    ) {
+        $rut = trim($_POST['rut']);
+        $consultaDuplicado = "SELECT * FROM persona WHERE rut = '$rut'";
+        $resultadoDuplicado = mysqli_query($conexion, $consultaDuplicado);
+        $existeDuplicado = mysqli_num_rows($resultadoDuplicado) > 0;
+
+        if ($existeDuplicado) {
+            ?>
+            <h3 class="error">Ya existe un registro con este RUT.</h3>
+            <?php
+        } else {
             $nombre_apellido = trim($_POST['nombre_apellido']);
             $alias = trim($_POST['alias']);
-            $rut = trim($_POST['rut']);
             $email = trim($_POST['email']);
             $region = trim($_POST['region']);
             $comuna = trim($_POST['comuna']);
             $candidato = trim($_POST['candidato']);
             $ComoSeEntero = is_array($_POST['metodo']) ? implode(", ", $_POST['metodo']) : '';
 
-            
-            $consulta = "INSERT INTO persona(nombre_apellido, alias, rut, email, region, comuna, candidato, como_se_entero	)
+            $consulta = "INSERT INTO persona(nombre_apellido, alias, rut, email, region, comuna, candidato, como_se_entero)
                 values ('$nombre_apellido','$alias','$rut','$email','$region','$comuna','$candidato','$ComoSeEntero')";
             $resultado = mysqli_query($conexion, $consulta);
-            if ($resultado){
+            if ($resultado) {
                 ?>
-                <h3 class = "success">registro completado</h3>
+                <h3 class="success">registro completado</h3>
                 <?php
-            }else{
+            } else {
                 ?>
-                <h3 class="error">ocurrio un error</h3>
-                <?php }
-        }else{
-            ?>
-            <h3 class="error">llena todos los campos</h3>
-            <?php
-        
+                <h3 class="error">ocurrió un error</h3>
+                <?php
+            }
+        }
+    } else {
+        ?>
+        <h3 class="error">llena todos los campos</h3>
+        <?php
     }
 }
-
 ?>
